886498f7167ed960582413653a3340cc1001e67e
Srinath318 <187srinath@gmail.com>
2025-11-16 10:16:38 +0530

    frontend final changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://dlsbackend-production.up.railway.app";
 let output = document.getElementById("output");
 
 // GET RANDOM SECRET
@@ -27,7 +27,7 @@ function postSecret() {
   }
 
   // If valid → continue posting
-  fetch(`${BASE_URL}/?secret=${secret}`, {
+  fetch(`${BASE_URL}/?secret=${encodeURIComponent(secret)}`, {
     method: "POST",
   })
     .then((res) => res.text())
